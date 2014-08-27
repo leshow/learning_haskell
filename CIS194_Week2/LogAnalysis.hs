90cab9167936709adcbb81a0beac77d45324ae31
Evan Cameron <cameron.evan@gmail.com>
2014-08-27 19:22:00 -0400

    parse message took me hours but finally is done

---
+++ CIS194_Week2/LogAnalysis.hs
@@ -0,0 +1,26 @@
+{- OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+{-
+ my first failed attempt:
+parseMessage :: String -> LogMessage
+parseMessage message = 
+	let typeM = case (head msg) of
+		"I" -> Info
+		"W" -> Warning
+		"E" -> Error (read (tail (head msg)))
+	in LogMessage typeM stamp rest 
+	where 	
+		msg = words message
+		stamp = read (head (tail msg))
+		rest  = unwords (tail (tail msg))
+parseMessage _ =  Unknown "This is not the right format"
+-}
+parseMessage :: String -> LogMessage
+parseMessage message = case words message of
+						("I":timestamp:rest) -> LogMessage Info (read timestamp) (unwords rest)
+						("W":timestamp:rest) -> LogMessage Warning (read timestamp) (unwords rest)
+						("E":err:timestamp:rest) -> LogMessage (Error (read err)) (read timestamp) (unwords rest)
+						msg -> Unknown "Wrong log message dummy"
