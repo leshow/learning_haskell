7249b56d2a233951d56f8a05cfcf9dcce5755c42
Evan Cameron <cameron.evan@gmail.com>
2014-08-30 02:14:36 -0400

    insert

--- CIS194_Week2/LogAnalysis.hs
+++ CIS194_Week2/LogAnalysis.hs
@@ -27,3 +27,17 @@ parseMessage message = case words message of
 
 parse :: String -> [LogMessage]
 parse content = map parseMessage $ lines content -- eq to -> map parseMessage . lines
+
+getTimestamp :: LogMessage -> Int 
+getTimestamp (LogMessage (Error _) stamp _)	= stamp
+getTimestamp (LogMessage _ stamp _)					= stamp
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree	= tree 			-- if the LogMessage was const with Unknown, return original MessageTree
+insert logX (Node treeA logY treeB)
+	|	x == y	= Node treeA logY treeB
+	| x > y		= Node (insert logY treeA) logX treeB
+	| x < y		= Node treeA logX (insert logY treeB)
+	where
+		x	= getTimestamp logX
+		y = getTimestamp logY 
