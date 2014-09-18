c4931d2188ba893566586698756eafa8249b262c
Evan Cameron <cameron.evan@gmail.com>
2014-09-18 15:01:32 -0400

    got part1

--- CIS194_Week3/golf.hs
+++ CIS194_Week3/golf.hs
@@ -1,16 +1,19 @@
 module Golf where
 --unfinished
 {-
+	FINALLY
+-}
 skips :: [a] -> [[a]]
 skips []		= []
 skips [x]		= [[x]]
-skips list		= foldr (\x -> everyN (x+1)) 1 list
+skips xs 		= map (everyN xs) [1..length xs] -- partially apply everyN with list xs
+												-- [1..length xs] -> makes a list from 1 to the length of list xs, this will be our values of n for everyN
+												-- map calls the partially applied everyN function onto that list
+
+{-
+	gets every nth element in a list, example everyN "ABCD" 2 = "BD"
 -}
-
-bigList list = bigList (doList 0 list)
-
-doList x list = everyN (x+1) list
-
-everyN n xs	= case drop (n-1) xs of
-				(z:zs) -> z : everyN n zs
+everyN:: [a] -> Int -> [a]
+everyN xs n	= case drop (n-1) xs of	-- drop the first part of the list up to n-1
 				[] -> []
+				(z:zs) -> z : everyN zs n -- cons nth element to list, call again with remaining
