35c21d01490992c7c6a022b7812a83adff63ff77
Evan Cameron <cameron.evan@gmail.com>
2014-09-17 23:03:25 -0400

    can't get it

--- CIS194_Week3/golf.hs
+++ CIS194_Week3/golf.hs
@@ -1,9 +1,15 @@
 module Golf where
 --unfinished
+{-
 skips :: [a] -> [[a]]
 skips []		= []
-skips [x]		= [x]
-skips list		= 
+skips [x]		= [[x]]
+skips list		= foldr (\x -> everyN (x+1)) 1 list
+-}
+
+bigList list = bigList (doList 0 list)
+
+doList x list = everyN (x+1) list
 
 everyN n xs	= case drop (n-1) xs of
 				(z:zs) -> z : everyN n zs
