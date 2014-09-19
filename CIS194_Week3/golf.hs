1ad0ce7ca786e580081e3c1b9d0ada9c5fa930d8
Evan Cameron <cameron.evan@gmail.com>
2014-09-19 16:14:35 -0400

    comments

--- CIS194_Week3/golf.hs
+++ CIS194_Week3/golf.hs
@@ -18,12 +18,13 @@ everyN xs n	= case drop (n-1) xs of	-- drop the first part of the list up to n-1
 				(z:zs) -> z : everyN zs n -- cons nth element to list, call again with remaining
 
 {- Exercise 2 
-n takes our triplet and returns true if the middle element is larger than both
-the lambda passed to map takes the middle element of the triplet and returns it
-the zip3 takes the list, a list starting at the second elem, and a list starting
+-the zip3 takes the list, a list starting at the second elem, and a list starting
 	at the third element. and creates a list of triplets with each of those values
 	so [1,2,3,4]
 	becomes: [(1,2,3),(2,3,4)]
+-n takes our triplet and returns true if the middle element is larger than both
+-the lambda passed to map takes the middle element of the triplet and returns it
+
 -}		
 localMaxima:: [Integer] -> [Integer]
 localMaxima list =  map (\(_,w,_) -> w) $ filter n $ zip3 list (tail list) (tail $ tail list)
