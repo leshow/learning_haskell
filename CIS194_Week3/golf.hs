52b302de529a960d3f76f01626d50a248c28273a
Evan Cameron <cameron.evan@gmail.com>
2014-09-19 16:06:31 -0400

    ex2

--- CIS194_Week3/golf.hs
+++ CIS194_Week3/golf.hs
@@ -1,7 +1,6 @@
 module Golf where
---unfinished
 {-
-	FINALLY
+	FINALLY -- Exercise 1
 -}
 skips :: [a] -> [[a]]
 skips []		= []
@@ -17,3 +16,16 @@ everyN:: [a] -> Int -> [a]
 everyN xs n	= case drop (n-1) xs of	-- drop the first part of the list up to n-1
 				[] -> []
 				(z:zs) -> z : everyN zs n -- cons nth element to list, call again with remaining
+
+{- Exercise 2 
+n takes our triplet and returns true if the middle element is larger than both
+the lambda passed to map takes the middle element of the triplet and returns it
+the zip3 takes the list, a list starting at the second elem, and a list starting
+	at the third element. and creates a list of triplets with each of those values
+	so [1,2,3,4]
+	becomes: [(1,2,3),(2,3,4)]
+-}		
+localMaxima:: [Integer] -> [Integer]
+localMaxima list =  map (\(q,w,e) -> w) $ filter n $ zip3 list (tail list) (tail $ tail list)
+	where 
+		n (j,k,l) = (k > j) && (k > l)
